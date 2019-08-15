510cfeff2b119def83473ace981a6f482fc18bcf
Unknown <dennis.groetzinger@web.de>
2019-08-15 10:31:58 +0200

    new CNN

--- influence/all_CNN_c.py
+++ influence/all_CNN_c.py
@@ -59,7 +59,7 @@ class All_CNN_C(GenericNeuralNet):
 
 
     def test_keras_model2(self):
-        model = Sequential()
+        '''model = Sequential()
         output_size = 32
         kernel_size = 3
         new_output_size = 0
@@ -83,13 +83,47 @@ class All_CNN_C(GenericNeuralNet):
         model.compile(loss="categorical_crossentropy",
                       optimizer=optimizer, metrics=['accuracy'])
 
+        return model'''
+        __neuron_list = [82, 253, 85, 60, 19, 32, 19]
+
+        l2reg = l2(0.04312)
+        model = Sequential()
+        output_size = 32
+        kernel_size = 3
+        layer_counter = 0
+
+        if 4 != 0:
+            model.add(Conv2D(__neuron_list[layer_counter], (kernel_size, kernel_size), input_shape=(128, 128, 1)))
+            model.add(Activation("tanh"))
+            layer_counter += 1
+
+            model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+
+        for i in range(1, 4):
+            model.add(Conv2D(__neuron_list[layer_counter], (kernel_size, kernel_size)))
+            model.add(Activation("tanh"))
+            layer_counter += 1
+
+            model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+
+        model.add(Flatten())
+        model.add(Dropout(0.1))
+
+        model.add(Dense(4))
+        model.add(Activation("sigmoid"))
+
+        optimizer = optimizers.SGD(0.04312)
+
+        model.compile(loss="mean_squared_error",
+                      optimizer=optimizer, metrics=['accuracy'])
+
         return model
 
 
 
     def get_initializers_of_keras2(self,model):
         #get the weights of each layer and flatten them
-        weights1 = model.layers[0].get_weights()[0]
+        '''weights1 = model.layers[0].get_weights()[0]
         weights1 = weights1.flatten()
         biases1 = model.layers[0].get_weights()[1]
 
@@ -105,20 +139,45 @@ class All_CNN_C(GenericNeuralNet):
         weights4 = weights4.flatten()
         biases4 = model.layers[7].get_weights()[1]
 
-        return [weights1,biases1,weights2,biases2,weights3,biases3,weights4,biases4]
+        return [weights1,biases1,weights2,biases2,weights3,biases3,weights4,biases4]'''
+        weights1 = model.layers[0].get_weights()[0]
+        weights1 = weights1.flatten()
+        biases1 = model.layers[0].get_weights()[1]
 
 
-    def conv2d_softplus(self, input_x, conv_patch_size, input_channels, output_channels, stride):
+        weights4 = model.layers[3].get_weights()[0]
+        weights4 = weights4.flatten()
+        biases4 = model.layers[3].get_weights()[1]
+
+
+        weights7 = model.layers[6].get_weights()[0]
+        weights7 = weights7.flatten()
+        biases7 = model.layers[6].get_weights()[1]
+
+
+        weights10 = model.layers[9].get_weights()[0]
+        weights10 = weights10.flatten()
+        biases10 = model.layers[9].get_weights()[1]
+
+
+        weights15 = model.layers[14].get_weights()[0]
+        weights15 = weights15.flatten()
+        biases15 = model.layers[14].get_weights()[1]
+
+        return [weights1, biases1, weights4, biases4, weights7, biases7, weights10, biases10,weights15,biases15]
+
+
+    def conv2d_softplus(self, input_x, conv_patch_size, input_channels, output_channels, index,stride):
         model2 = self.test_keras_model2()
         initializers = self.get_initializers_of_keras2(model2)
 
         weights = tf.get_variable(
             'weights',
-            initializer= initializers [0],
+            initializer= initializers [index],
             dtype = tf.float32)
         biases = tf.get_variable(
             'biases',
-            shape=[32],
+            initializer=initializers[index + 1],
             dtype=tf.float32)
         weights_reshaped = tf.reshape(weights, [conv_patch_size, conv_patch_size, input_channels, output_channels])
         hidden = tf.nn.tanh(conv2d(input_x, weights_reshaped, stride) + biases)
@@ -127,9 +186,8 @@ class All_CNN_C(GenericNeuralNet):
 
     def get_all_params(self):
 
-
         all_params = []
-        for layer in ['conv1', 'dense1','dense2','dense3']:
+        for layer in ['conv1', 'conv2','conv3','conv4','dense1']:
             for var_name in ['weights', 'biases']:
                 temp_tensor = tf.get_default_graph().get_tensor_by_name("%s/%s:0" % (layer, var_name))
                 all_params.append(temp_tensor)
@@ -156,9 +214,53 @@ class All_CNN_C(GenericNeuralNet):
             name='labels_placeholder')
         return input_placeholder, labels_placeholder
 
+    def maxpool2d(x, k=2):
+        return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+
 
     def inference(self, input_x):
-        model2 = self.test_keras_model2()
+        model2 = keras.models.load_model("data/final_net.h5")
+        initializers = self.get_initializers_of_keras2(model2)
+        input_reshaped = tf.reshape(input_x, [-1, self.input_side, self.input_side, self.input_channels])
+        last_layer_units = 128
+
+        with tf.variable_scope('conv1'):
+            # variablen definieren und conv und relu alles in einem schritt
+            conv1 = self.conv2d_softplus(input_reshaped, self.conv_patch_size, self.input_channels, 82, 0,stride=1)
+        conv1 = self.maxpool2d(conv1)
+
+        with tf.variable_scope('conv2'):
+            # variablen definieren und conv und relu alles in einem schritt
+            conv2 = self.conv2d_softplus(conv1, self.conv_patch_size, self.input_channels, 253,2, stride=1)
+
+        conv2 = self.maxpool2d(conv2)
+        with tf.variable_scope('conv3'):
+            # variablen definieren und conv und relu alles in einem schritt
+            conv3 = self.conv2d_softplus(conv2, self.conv_patch_size, self.input_channels, 85,4, stride=1)
+        conv3 = self.maxpool2d(conv3)
+
+        with tf.variable_scope('conv4'):
+            # variablen definieren und conv und relu alles in einem schritt
+            conv4 = self.conv2d_softplus(conv3, self.conv_patch_size, self.input_channels, 60,6, stride=1)
+
+        conv4 = self.maxpool2d(conv4)
+
+        conv_reshaped = tf.reshape(conv4, [-1, (self.input_side - 2) * (self.input_side - 2) * 60])
+
+        with tf.variable_scope('dense1'):
+            # definition der weights
+            weights1 = tf.get_variable(
+                'weights',
+                initializer=initializers[8])
+            # def der biases
+            biases1 = tf.get_variable(
+                'biases',
+                initializer=initializers[9])
+            dense1 = tf.matmul(conv_reshaped, tf.reshape(weights1, [(self.input_side - 2) * (self.input_side - 2) * 60,
+                                                                     self.num_classes])) + biases1
+
+        return dense1
+        '''model2 = self.test_keras_model2()
         initializers = self.get_initializers_of_keras2(model2)
 
         #input, which is a vector gets reshaped to an image
@@ -209,7 +311,8 @@ class All_CNN_C(GenericNeuralNet):
 
             dense3 = tf.matmul(dense2, tf.reshape(weights3, [last_layer_units, self.num_classes])) + biases3
 
-        return dense3
+        return dense3'''
+
 
     def predictions(self, logits):
         preds = tf.nn.softmax(logits, name='preds')
